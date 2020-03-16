c3279271698b0cd7fb1bece23a687e43b2780b0f
Leonardo Mota <contact@leoism.me>
2020-03-16 16:34:15 -0700

    Added Magic8

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const keys = require("./keys.js");
+const magic8 = require("./functions/magic8.js");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  let regex = /[!#.\/](?:magic8|8|ask)/;
+  console.log(JSON.stringify(msg, null, 2));
+  if (msg.content.match(regex)) {
+    msg.channel.send(magic8.ask());
   }
 });
 
